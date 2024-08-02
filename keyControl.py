1c8f7007bd1edebf45bdf02b4762cc3727e14e5f
ChayNick2010 <boryach2010@gmail.com>
2024-08-02 13:47:52 +0300

    рабочая версия 1.0

--- keyControl.py
+++ keyControl.py
@@ -1,9 +1,9 @@
-from control import key_press, SC_DOWN, SC_UP,SC_RIGHT,SC_LEFT, key_down, SC_INSERT, SC_DELETE
+from control import key_press, SC_DOWN, SC_UP,SC_RIGHT,SC_LEFT, key_down
 def control_car(func):
     key_down(SC_UP)
     cof = func // 3300
     speed = func // 3000
-    fast = 2
+
     if func > 0:
         key_press(SC_RIGHT, interval=abs(cof))
         key_press(SC_LEFT, interval=abs(cof)//2)
@@ -15,16 +15,3 @@ def control_car(func):
         key_press(SC_RIGHT, interval=abs(cof) // 2)
         if func < -250:
             key_press(SC_DOWN, interval=abs(speed))
-
-    if func < 20 and func > -20:
-        key_press(SC_INSERT, interval=0.02)
-        if fast == (2, 3, 4, 5):
-            fast + 1
-        if fast == (6,7):
-            fast + 0
-    if func > 50 and fast == (4, 5, 6, 7):
-        key_press(SC_DELETE, interval=0.02)
-        fast - 1
-    if func < -50 and fast == (4, 5, 6, 7):
-        key_press(SC_DELETE, interval=0.02)
-        fast - 1
